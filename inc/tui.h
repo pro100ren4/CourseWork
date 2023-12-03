a745c7921f01e152988db2ac7d73c48a6ef75b94
pro100ren4 <78037325+pro100ren4@users.noreply.github.com>
2023-12-04 02:12:32 +0300

    Сделал функции для работы с главным меню

--- inc/tui.h
+++ inc/tui.h
@@ -1,41 +1,36 @@
 #ifndef _TUI_H_
 #define _TUI_H_
 
-#define BORDER "┌─────┬────────────────────┬────────┬───┬──────┬────────┬────────────────┐\n"\
-"│PN   │SURNAME             │THEME   │DOF│SALARY│JC      │DN              │\n"\
-"├─────┴────────────────────┴────────┴───┴──────┴────────┴────────────────┤\n"\
-"│1     Mudrik                                                            │\n"\
-"│2     Usmanskiy                                                         │\n"\
-"│3     Purgin                                                            │\n"\
-"│4                                                                       │\n"\
-"│5                                                                       │\n"\
-"│6                                                                       │\n"\
-"│7                                                                       │\n"\
-"│8                                                                       │\n"\
-"│9                                                                       │\n"\
-"│10                                                                      │\n"\
-"├────────────────────────────────────────────────────────────────────────┤\n"\
-"│MORE ABOUT FIELD                                                        │\n"\
-"└────────────────────────────────────────────────────────────────────────┘\n"\
-" TIPS: [x] exit; [f12] more;                                              \n"
-
-/*!
- * USAGE:
- * Only read, do not write anything into this
-*/
-static int term_height;
-static int term_width;
+#define menu_line_size 32
 
 void initialize_term_xy(int *x, int *y);
-// {
-//     printf("\x1B[255;255H\x1B[6n");
-//     scanf("\x1B[%d;%dR", &term_width, &term_height);
-//     printf("Width: %d; Height: %d\n", term_width, term_height);//DE:ETE in Release
-// }s
+
+struct menu_t {
+    char **menu_text;
+    int menu_size;
+    int chosen;
+    int x, y;
+    int (*menu_callback)(struct menu_t *);
+    struct menu_t *subs;
+};
+
+struct menu_t create_menu(char **menu_text, int menu_size, int (*menu_callback)(struct menu_t *), int x, int y);
+void delete_menu(struct menu_t *menu);
+
+void add_menu_sub(struct menu_t *main_menu, struct menu_t *sub_menu, int position);
+
+void print_menu(struct menu_t *menu);
+// int check_menu_input(struct menu_t *menu);
+// void foo(void);
+void menu_process(struct menu_t *menu);
+// int check_main_menu(struct menu_t *menu);
 
 
 void draw_correct_form(int width, int height);
 void draw_table_form(int width, int height);
 void draw_menu_form(int width, int height);
 void draw_help_form(int width, int height);
+
+void set_keypress(void);
+void reset_keypress(void);
 #endif //_TUI_H_
--- src/tui.c
+++ src/tui.c
@@ -1,19 +1,20 @@
 #include "../inc/tui.h"
 
+#include <termios.h>
+#include <stdlib.h>
+
 #include "../inc/utils.h"
 
-/*
- * Need to type [Enter] when use
-*/
+//Need to type [Enter] when use
 void initialize_term_xy(int *x, int *y) {
     printf("\x1B[255;255H\x1B[6n");
-    scanf("\x1B[%d;%dR", &term_height, &term_width);
+    scanf("\x1B[%d;%dR", x, y);
     clrscr();
     home();
-    printf("Width: %d, Height: %d\n", term_width, term_height);
-    *x = term_width;
-    *y = term_height;
-    system(PAUSE);
+    // printf("Width: %d, Height: %d\n", term_width, term_height);
+    // *x = term_width;
+    // *y = term_height;
+    // system(PAUSE);
 }
 
 // Use in cycle
@@ -37,6 +38,25 @@ void draw_correct_form(int width, int height) {
         printf(CORRECT_FORM[i]);
     }
 }
+
+
+// "┌─────┬────────────────────┬────────┬───┬──────┬────────┬────────────────┐\n"\
+// "│PN   │SURNAME             │THEME   │DOF│SALARY│JC      │DN              │\n"\
+// "├─────┴────────────────────┴────────┴───┴──────┴────────┴────────────────┤\n"\
+// "│1     Mudrik                                                            │\n"\
+// "│2     Usmanskiy                                                         │\n"\
+// "│3     Purgin                                                            │\n"\
+// "│4                                                                       │\n"\
+// "│5                                                                       │\n"\
+// "│6                                                                       │\n"\
+// "│7                                                                       │\n"\
+// "│8                                                                       │\n"\
+// "│9                                                                       │\n"\
+// "│10                                                                      │\n"\
+// "├────────────────────────────────────────────────────────────────────────┤\n"\
+// "│MORE ABOUT FIELD                                                        │\n"\
+// "└────────────────────────────────────────────────────────────────────────┘\n"\
+// " TIPS: [x] exit; [f12] more;                                              \n"
 
 void draw_table_form(int width, int height) {
     clrscr();
@@ -180,25 +200,36 @@ void draw_table_form(int width, int height) {
 // └────────────────────────────────────────┘0
 
 
-void draw_menu_form(int width, int height) {
-    int x = (width - 42)/2;
-    int y = 4;
-    char *CORRECT_FORM[] = {
-        "┌──────────────────MENU──────────────────┐",  
-        "│ [1] Read Data from File                │", 
-        "│ [2] Write Data to File                 │", 
-        "│ [3] Read Data from Console             │", 
-        "│ [4]                                    │", 
-        "│ [5]                                    │", 
-        "│                                        │", 
-        "│                                        │", 
-        "│ [x] Close                              │", 
-        "└────────────────────────────────────────┘"  
-    };
-    for (int i = 0; i < 10; i++) {
-        cursor_to_xy(x,y + i);
-        printf(CORRECT_FORM[i]);
+void draw_menu_form(int x, int y) {
+    clrscr();
+    home();
+
+    //int x = (width - 34)/2, y = (height - 7)/2;
+
+    cursor_to_xy(x, y);
+
+    printf("┌MENU");
+    for (int i = 6; i < 34; i++) {
+        printf("─");
     }
+    printf("┐\n");
+
+    for (int i = 1; i < 6; i++) {
+        cursor_to_xy(x, y + i);
+        printf("│");
+        for (int j = 0; j < 34 - 2; j++) {
+            printf(" ");
+        }
+        printf("│\n");
+    }
+
+    cursor_to_xy(x, y + 6)
+
+    printf("└");
+    for (int i = 2; i < 34; i++) {
+        printf("─");
+    }
+    printf("┘\n");
 }
 
 // 123456789012345678901234567890123456789012  42
@@ -233,3 +264,100 @@ void draw_help_form(int width, int height) {
         printf(CORRECT_FORM[i]);
     }
 }
+
+static struct termios stored_settings;
+
+void set_keypress(void)
+{
+    struct termios new_settings;
+
+    tcgetattr(0,&stored_settings);
+
+    new_settings = stored_settings;
+
+    /* Disable canonical mode, and set buffer size to 1 byte */
+    new_settings.c_lflag &= (~ICANON);
+    new_settings.c_cc[VTIME] = 0;
+    new_settings.c_cc[VMIN] = 1;
+
+    tcsetattr(0,TCSANOW,&new_settings);
+    return;
+}
+
+void reset_keypress(void)
+{
+    tcsetattr(0,TCSANOW,&stored_settings);
+    return;
+}
+
+
+
+
+void print_menu(struct menu_t *menu) {
+    for (int i = 0; i < menu->menu_size; i++) {
+        cursor_to_xy(menu->x, menu->y + i);
+        if(i == menu->chosen)
+            set_display_atrib(REVERSE);
+        printf("%-*s", menu_line_size, menu->menu_text[i]);
+        if(i == menu->chosen)
+            resetcolor();
+    }
+}
+
+
+void menu_process(struct menu_t *menu) {
+    
+    char act;
+    //menu->chosen = -1;
+    
+    do {
+        if (act == 106 && menu->chosen < menu->menu_size) { 
+            //J
+            menu->chosen++;
+        } else if (act == 107 && menu->chosen >= 0) {
+            //K
+            menu->chosen--;
+        } else if (act == 10) {
+            //ENTER
+            menu->menu_callback(menu);
+        } else if (act == 27) {
+            //ESC
+            return;
+        } else if (act >= 49 && act <= 58) {
+            menu->chosen = act - 49;
+            menu->menu_callback(menu);
+        } else if (act == 48) {
+            menu->chosen = 10;
+            menu->menu_callback(menu);
+        } 
+
+        draw_menu_form(menu->x - 1, menu->y - 1);
+        print_menu(menu);
+        act = getchar();
+
+    } while (1);
+}
+
+
+struct menu_t create_menu(char **menu_text, int menu_size, int (*menu_callback)(struct menu_t *), int x, int y) {
+    struct menu_t menu = {
+        .menu_text = menu_text, 
+        .menu_size = menu_size,
+        .menu_callback = menu_callback,
+        .subs = (struct menu_t *)malloc(sizeof(struct menu_t)*menu_size),
+        .chosen = -1,
+        .x = x,
+        .y = y
+    };
+    return menu;
+}
+
+
+void delete_menu(struct menu_t *menu) {
+    free(menu->subs);
+}
+
+void add_menu_sub(struct menu_t *main_menu, struct menu_t *sub_menu, int position) {
+    main_menu->subs[position] = *sub_menu;
+}
+
